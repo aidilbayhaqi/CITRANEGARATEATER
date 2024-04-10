0c0aa64a4a781eb3a6ce0bb8706d51a7633dacf2
aidilbyhaqi <aidilbyhaqi>
2024-04-10 14:48:50 +0700

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,8 @@ import { createBrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    
+    <createBrowserRouter>
       <App />
-
+</createBrowserRouter>
   </React.StrictMode>
 );
